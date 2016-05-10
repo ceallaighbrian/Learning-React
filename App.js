84cdf5173d2ed88fe819cd886fcd767a91da233c
Brian Kelly <ceallaighbrian@gmail.com>
2016-05-10 21:22:41 +0100

    Lesson 6 Owner Ownee Relationship

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ class App extends React.Component {
         this.state = {
             txt: "This is the state txt"
         }
+        this.update = this.update.bind(this)
     }
 
     update(e) {
@@ -17,12 +18,24 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <input type="text"
-                       onChange={this.update.bind(this)}/>
-                <h1>{this.state.txt}</h1>
+                <Widget txt={this.state.txt}
+                update = {this.update}/>
+                <Widget txt={this.state.txt}
+                update = {this.update}/>
+                <Widget txt={this.state.txt}
+                update = {this.update}/>
             </div>
         )
     }
+}
+
+const Widget = (props) => {
+    return (
+        <div>
+            <input type="text" onChange={props.update}/>
+            <h1>{props.txt}</h1>
+        </div>
+    )
 }
 
 
