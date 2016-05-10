1567e58d67ac6e5e130ef1b85e60378415831381
Brian Kelly <ceallaighbrian@gmail.com>
2016-05-10 20:33:37 +0100

    Lesson 4 Properties

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+    render() {
+        let txt = this.props.txt
+        return <h1>{txt}</h1>
+
+    }
+}
+
+App.propTypes = {
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+    txt: "this is default"
+}
+
+ReactDOM.render(
+    <App cat={5} txt="props test"/>,
+    document.getElementById('app')
+)
+
+
+export default App
