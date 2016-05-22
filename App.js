834bc3b0fe8b111ea449cfaef69fa816baeff1a1
Brian Kelly <ceallaighbrian@gmail.com>
2016-05-23 00:03:49 +0100

    Lesson 9 mounting basics

--- App.js
+++ App.js
@@ -1,59 +1,49 @@
-import React from "react";
-import ReactDom from 'react-dom';
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 class App extends React.Component {
-
-    constructor() {
+    constructor(){
         super();
-        this.state = {
-            red: 0,
-            green: 0,
-            blue: 0
-        }
-
-        this.update = this.update.bind(this)
+        this.state = { val: 0 };
+        this.update = this.update.bind(this);
     }
-
-    update(e) {
-        this.setState({
-        red: ReactDom.findDOMNode(this.refs.red.refs.inp).value,
-            green: ReactDom.findDOMNode(this.refs.green.refs.inp).value,
-            blue: ReactDom.findDOMNode(this.refs.blue.refs.inp).value
-        })
+    update(){
+        this.setState({val: this.state.val + 1 })
     }
+    componentWillMount(){
+        console.log('mounting')
+    }
+    render(){
+        console.log('rendering')
+        return <button onClick={this.update}>{this.state.val}</button>
+    }
+    componentDidMount(){
+        console.log('mounted')
+    }
+    componentWillUnmount(){
+        console.log('gluck')
+    }
+}
 
-    render() {
+class Wrapper extends React.Component {
+    constructor(){
+        super();
+    }
+    mount(){
+        ReactDOM.render(<App />, document.getElementById('a'))
+    }
+    unmount(){
+        ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+    }
+    render(){
         return (
             <div>
-                <Slider ref="red" update = {this.update}/>
-                {this.state.red}
-                <br />
-                <Slider ref="green" update = {this.update}/>
-                {this.state.green}
-                <br />
-                <Slider ref="blue" update = {this.update}/>
-                {this.state.blue}
-                <br />
+                <button onClick={this.mount.bind(this)}>Mount</button>
+                <button onClick={this.unmount.bind(this)}>Unmount</button>
+                <div id="a"></div>
             </div>
         )
     }
 }
 
-class Slider extends React.Component {
-    render() {
 
-        return (
-            <div>
-                <input ref="inp" type="range"
-                       min="0"
-                       max="255"
-                       onChange={this.props.update}/>
-            </div>
-        );
-    }
-}
-
-
-
-export default App
+export default Wrapper
